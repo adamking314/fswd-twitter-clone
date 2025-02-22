baa805f71df963d7d2edb2e9905e2324ff12f606
adamking314 <king.adam314@gmail.com>
2025-02-22 15:33:22 -0800

    first push

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -4,7 +4,16 @@ import ReactDOM from 'react-dom'
 import './home.scss';
 
 const Home = props => (
+  <div>
   <h1>Home page react is working</h1>
+  <div className="col-xs-12 post-tweet-box">
+        <textarea type="text" className="form-control post-input" rows="3" placeholder="What's happening?"></textarea>
+        <div className="pull-right">
+          <span className="post-char-counter">140</span>
+          <button className="btn btn-primary" disabled id="post-tweet-btn">Tweet</button>
+        </div>
+      </div>
+  </div>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
