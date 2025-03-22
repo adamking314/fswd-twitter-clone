cc9e09e556e106b0f4a456f388b37176b6534e21
adamking314 <king.adam314@gmail.com>
2025-03-22 15:00:51 -0700

    hoppefully fixing feed page erro

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2025_03_14_190437) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,14 +39,14 @@ ActiveRecord::Schema.define(version: 2025_03_14_190437) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
@@ -50,7 +54,7 @@ ActiveRecord::Schema.define(version: 2025_03_14_190437) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
